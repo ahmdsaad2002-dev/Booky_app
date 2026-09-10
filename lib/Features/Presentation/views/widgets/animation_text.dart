04590c8e59d4f0f50eb8671e321e2d48f255353d
Ahmed saad <ahmdsaad2002@gmail.com>
2026-09-11 01:35:52 +0300

    add sliding animation and fading animation to the splash screen

---
+++ lib/Features/Presentation/views/widgets/animation_text.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
+
+class AnimationText extends StatelessWidget {
+  const AnimationText({
+    super.key,
+    required AnimationController animationController,
+    required this.slidingAnimation,
+    required this.opacityAnimation,
+  }) : _animationController = animationController;
+
+  final AnimationController _animationController;
+  final Animation<Offset> slidingAnimation;
+  final Animation<double> opacityAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: FadeTransition(
+              opacity: opacityAnimation,
+              child: const Text(
+                "Read Free Books",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          );}
+    );
+  }
+}
--- lib/Features/Presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import '../../../../Core/app_images.dart';
+import 'animation_text.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> slidingAnimation;
+  late Animation<double> opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 10),
+      end: Offset.zero,
+    ).animate(_animationController);
+    _animationController.forward();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+    opacityAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(_animationController);
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +57,10 @@ class SplashViewBody extends StatelessWidget {
             child: SvgPicture.asset(AppImages.logo, width: 75, height: 75),
           ),
           SizedBox(height: 35),
-          const Text(
-            "Read Free Books",
+          AnimationText(
+            animationController: _animationController,
+            slidingAnimation: slidingAnimation,
+            opacityAnimation: opacityAnimation,
           ),
         ],
       ),
